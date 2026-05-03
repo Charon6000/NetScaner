143870e56eb5a6b112b68dd8fbef7598ac2009fb
Charon6000 <hubertwit2005@gmail.com>
2026-05-03 21:57:27 +0200

    Dzialajace getopts #9

--- functions.sh
+++ functions.sh
@@ -3,17 +3,17 @@
 # Skanowanie sieci lokalnej w celu wykrycia aktywnych urządzeń.
 scanDevices()
 {
-
+    echo "Szukanie Urzadzen"    
 }
 
 # Wykrywanie otwartych portów na urządzeniach w sieci.
 scanOpenPorts()
 {
-
+    echo "Szukanie Portow"
 }
 
 # Generowanie Raportu
 generateRaport()
 {
-    
+    echo "Generowanie Raportu"
 }
--- main.sh
+++ main.sh
@@ -1,7 +1,25 @@
 #!/bin/bash
 
+# Zaimportowanie funkcjonalności z pliku functions.sh
+
+source ./functions.sh
+
+#Wersja programu
+VERSION="1.0.0"
+
+# Obsluga getopts
+
+while getopts "hvr" opt; do
+    case $opt in
+    h) echo "help"; exit 0 ;;
+    v) echo "NetScanner: $VERSION"; exit 0 ;;
+    r) echo "run"; exit 0 ;;
+    *) echo "help"; exit 0 ;;
+    esac
+done
+
 # Obsluga GUI i interakcji z użytkownikiem.
 
-menu=("Skanuj urządzenia" "Skanuj porty")
-odp=$(zenity --list --column=Menu "${menu[@]}" --height 170)
-echo "Wybrano: $odp"
+# menu=("Skanuj urządzenia" "Skanuj porty")
+# odp=$(zenity --list --column=Menu "${menu[@]}" --height 170)
+# echo "Wybrano: $odp"
